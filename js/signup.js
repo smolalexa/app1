508a289671a3907078cb51914d2d5e6549796dd6
Oleksandr Smolianov <smolianov.alexander@gmail.com>
2022-05-06 22:02:17 +0300

    Add signup.js

---
+++ js/signup.js
@@ -0,0 +1,11 @@
+const btnSignUp = document.querySelector('#signup');
+
+btnSignUp.addEventListener('click', e => {
+    fetch('/signup.php', {
+        method: 'POST',
+        headers: {
+            '': ''
+        }
+    }).then(res => res.text())
+
+})
